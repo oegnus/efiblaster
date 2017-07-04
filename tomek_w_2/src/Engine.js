4a55ab63d9287a8de213b03c1447496134026762
Tomasz Wiencek <twiencek@artegence.com>
2017-07-04 21:08:32 +0200

    added test for moving left

--- tomek_w_2/src/Engine.js
+++ tomek_w_2/src/Engine.js
@@ -1,14 +1,25 @@
 export const Engine = {
   tick: (getPlayerInputFn, gameState) => {
-    // const p1 = gameState.players[0];
-    // const newPlayerPosition = {
-    //   x: p1.x + 0.1,
-    //   y: p1.y
-    // };
-    // const tile = gameState.map.getTile(newPlayerPosition);
-    // if (tile.type === 'g') {
-    //   p1.x = newPlayerPosition.x;
-    //   p1.y = newPlayerPosition.y;
-    // }
+    const playerSpeed = 0.1;
+
+    const p0 = gameState.players[0];
+    const p0_input = getPlayerInputFn(0);
+
+    const p0_positionDelta = {x: 0, y: 0};
+    if (p0_input.direction.x > 0) {
+      p0_positionDelta.x = playerSpeed;
+    }
+    if (p0_input.direction.x < 0) {
+      p0_positionDelta.x = -playerSpeed;
+    }
+    const p0_newPosition = {
+      x: Math.round(100 * (p0.x + p0_positionDelta.x)) * 0.01,
+      y: Math.round(100 * (p0.y + p0_positionDelta.y)) * 0.01
+    };
+    const tile = gameState.map.getTile(p0_newPosition);
+    if (tile.type === 'g') {
+      p0.x = p0_newPosition.x;
+      p0.y = p0_newPosition.y;
+    }
   }
 };
