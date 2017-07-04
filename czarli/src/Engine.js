6b48437e2897e2e06eace7ca2d1a307febb773c9
cbajera <cbajera@artegence.com>
2017-07-04 20:54:48 +0200

    input commit

---
+++ czarli/src/Engine.js
@@ -0,0 +1,5 @@
+export const Engine = {
+  tick: (getPlayerInputFn, gameState) => {
+
+  }
+};
---
+++ czarli/test/Engine.spec.js
@@ -0,0 +1,22 @@
+const assert = require('assert');
+import 'babel-polyfill';
+import { Map } from '../src/Map';
+import { GameState } from '../src/GameState';
+import { Engine } from '../src/Engine';
+
+describe('Engine', function () {
+  it('should tick and not change players positions', function () {
+    const map = Map.createNewMap([]);
+    const players = [{x: 0, y: 0}];
+    const gameState = GameState.getNewGameState(map, players);
+    const getPlayerInput = () => {
+      return {
+        direction: {x: 0, y: 0}
+      };
+    };
+
+    Engine.tick(getPlayerInput, gameState);
+
+    assert.deepEqual(gameState.players, [{x: 0, y: 0}]);
+  });
+});
