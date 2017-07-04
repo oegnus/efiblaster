ae27db3c860568332d631cbfcb725cc8516c21cd
Tomasz Wiencek <twiencek@artegence.com>
2017-07-04 20:27:10 +0200

    added Input test

--- tomek_w_2/test/GameState.spec.js
+++ tomek_w_2/test/GameState.spec.js
@@ -1,10 +1,10 @@
-var assert = require('assert');
+const assert = require('assert');
 import 'babel-polyfill';
 import { GameState } from '../src/GameState';
 import { Map } from '../src/Map';
 
-describe('GameState', function() {
-  it('should return new game state', function() {
+describe('GameState', function () {
+  it('should return new game state', function () {
     const player = {x: 2, y: 3};
     const map = Map.createNewMap([]);
     const gameState = GameState.getNewGameState(
