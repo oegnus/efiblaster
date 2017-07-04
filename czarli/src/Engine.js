bccd4cd0d17b85d69ef1377423b8c42c081ec45d
cbajera <cbajera@artegence.com>
2017-07-04 21:30:04 +0200

    input commit

--- czarli/src/Engine.js
+++ czarli/src/Engine.js
@@ -1,9 +1,9 @@
 class _Engine {
   tick(getPlayerInputFn, gameState) {
-    let input = getPlayerInputFn();
+    let input = getPlayerInputFn(0);
     if (input.direction) {
-      gameState.players[0].x = input.direction.x / 10;
-      gameState.players[0].y = input.direction.y / 10;
+      gameState.players[0].x += input.direction.x / 10;
+      gameState.players[0].y += input.direction.y / 10;
     }
   }
 }
