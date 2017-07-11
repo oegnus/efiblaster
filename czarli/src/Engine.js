3a787b7f1cb5e671acbaacab503d7108cc7c0f45
cbajera <cbajera@artegence.com>
2017-07-11 18:35:28 +0200

    Engine tests

--- czarli/src/Engine.js
+++ czarli/src/Engine.js
@@ -2,8 +2,8 @@ class _Engine {
   tick(getPlayerInputFn, gameState) {
     let input = getPlayerInputFn(0);
     if (input.direction) {
-      gameState.players[0].x += input.direction.x / 10;
-      gameState.players[0].y += input.direction.y / 10;
+      gameState.players[0].x = (gameState.players[0].x * 10 + input.direction.x)/ 10;
+      gameState.players[0].y = (gameState.players[0].y * 10 + input.direction.y)/ 10;
     }
   }
 }
