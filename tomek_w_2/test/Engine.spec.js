d28251e714fc7339e97b53e600a26d153f53bbac
Tomasz Wiencek <twiencek@artegence.com>
2017-07-11 18:42:38 +0200

    added movement tests

--- tomek_w_2/test/Engine.spec.js
+++ tomek_w_2/test/Engine.spec.js
@@ -99,4 +99,27 @@ describe('Engine', function () {
 
     assert.deepEqual(gameState.players, [{x: 1, y: 1.2}]);
   });
+
+  it('shouldn\'t move outside of map', function () {
+    const map = Map.createNewMap([
+      'ggg',
+      'ggg',
+      'ggg',
+    ]);
+    const players = [{x: 0, y: 0}];
+    const gameState = GameState.getNewGameState(map, players);
+    let direction = {x: -1, y: 0};
+    const getPlayerInput = () => () => {
+      direction: direction
+    };
+
+    direction = {x: -1, y: 0};
+    Engine.tick(getPlayerInput, gameState);
+    Engine.tick(getPlayerInput, gameState);
+    direction = {x: 0, y: -1};
+    Engine.tick(getPlayerInput, gameState);
+    Engine.tick(getPlayerInput, gameState);
+
+    assert.deepEqual(gameState.players, [{x: 0, y: 0}]);
+  });
 });
