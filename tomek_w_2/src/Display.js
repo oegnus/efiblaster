ab7cf514a11c256a38d43212724b92fac8e978b6
Tomasz Wiencek <twiencek@artegence.com>
2017-07-11 20:17:23 +0200

    updated display

--- tomek_w_2/src/Display.js
+++ tomek_w_2/src/Display.js
@@ -1,10 +1,3 @@
-const tilesetAsset = document.getElementById("tileset");
-const playerAsset = document.getElementById("player");
-const c = document.getElementById("efiCanvas");
-const ctx = c.getContext("2d");
-
-const tileSize = 33;
-
 export const Display = {
   drawGameState: function (gameState) {
     this.drawMap(gameState.map);
@@ -21,39 +14,77 @@ export const Display = {
   }
 };
 
-function drawPlayer(player) {
-  ctx.drawImage(
-    // tileset:
-    playerAsset,
-    0,
-    0,
-    50,
-    44,
-    // canvas:
-    player.x * tileSize,
-    player.y * tileSize,
-    50,
-    44
+const tileSize = 1;
+
+var canvas = document.getElementById("renderCanvas");
+var engine = new BABYLON.Engine(canvas, true);
+const boxesDict = {};
+const playersDict = {};
+let drawTile = () => null;
+let drawPlayer = () => null;
+var createScene = function () {
+  var scene = new BABYLON.Scene(engine);
+  var camera = new BABYLON.ArcRotateCamera(
+    "Camera",
+    0.9 * Math.PI / 2,
+    1.5 * Math.PI / 2,
+    10,
+    new BABYLON.Vector3(-3, -3, 0),
+    scene
   );
+  var light = new BABYLON.HemisphericLight("hemi", new BABYLON.Vector3(5, 5, -2), scene);
+
+  var grassTexture = new BABYLON.StandardMaterial("grassTexture", scene);
+  grassTexture.diffuseTexture = new BABYLON.Texture("assets/grass.jpg", scene);
+
+  var rockTexture = new BABYLON.StandardMaterial("rockTexture", scene);
+  rockTexture.diffuseTexture = new BABYLON.Texture("assets/rock.jpg", scene);
+
+  var playerTexture = new BABYLON.StandardMaterial("playerTexture", scene);
+  playerTexture.diffuseTexture = new BABYLON.Texture("assets/1forw0.png", scene);
+  playerTexture.diffuseTexture.hasAlpha = true;
+  playerTexture.backFaceCulling = false;
+
+  drawTile = (tile) => {
+    const key = tile.x + '_' + tile.y;
+    if (!boxesDict[key]) {
+      const box = BABYLON.Mesh.CreateBox("box", 0.9, scene);
+      box.position = new BABYLON.Vector3(
+        -tile.x * tileSize,
+        -tile.y * tileSize,
+        (tile.type === 'g') ? 0 : 0.6
+      );
+      box.material = (tile.type === 'g') ? grassTexture : rockTexture;
+      boxesDict[key] = box;
+    }
+  };
+
+  drawPlayer = (player, playerId) => {
+    const key = playerId;
+    if (!playersDict[key]) {
+      const box = BABYLON.Mesh.CreateBox("box", 0.6, scene);
+      box.material = playerTexture;
+      playersDict[key] = box;
+    }
+    const playerMesh = playersDict[key];
+    console.log('playerMesh', playerMesh);
+    playerMesh.position = new BABYLON.Vector3(
+      -player.x * tileSize,
+      -player.y * tileSize,
+      1
+    );
+  };
+
+  return scene;
 }
 
-function drawTile(tile) {
-  let positionInTileset = {x: 0, y: 0};
-  if (tile.type === 'g') {
-    positionInTileset = {x: 2, y: 0};
-  }
+var scene = createScene();
 
-  ctx.drawImage(
-    // tileset:
-    tilesetAsset,
-    positionInTileset.x * tileSize,
-    positionInTileset.y * tileSize,
-    tileSize,
-    tileSize,
-    // canvas:
-    tile.x * tileSize,
-    tile.y * tileSize,
-    tileSize,
-    tileSize
-  );
-}
+engine.runRenderLoop(function () {
+    scene.render();
+});
+
+// Resize
+window.addEventListener("resize", function () {
+    engine.resize();
+});
